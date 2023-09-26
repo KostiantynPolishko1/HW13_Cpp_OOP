8a2404e63d50a64d3fe4393252f88e0775d0729d
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-09-26 07:18:20 +0300

    Revert "2 Done class 'WristWatch'."
    This reverts commit 6d31a87072fba296bea5258ed695dd05ed78adb3.

--- HW13_Cpp_OOP.cpp
+++ HW13_Cpp_OOP.cpp
@@ -1,23 +1,8 @@
 #include <iostream>
-#include "Watch.h"
-#include "WristWatch.h"
 
 int main()
 {
     std::cout << "Start!\n";
-   /* Watch watch1;
-    std::cout << watch1.getTime() << "\n";
 
-    Watch watch2(22, 13, (char*)"CASIO");
-    std::cout << watch2.getTime() << "\n";*/
-
-    /*WristWatch wristwatch1;
-    std::cout << wristwatch1.getDeepWater() << "\n";
-    std::cout << (wristwatch1.getWaterProof() ? "yes" : "no") << "\n";*/
-    
-    WristWatch wristwatch2(15, 22, 13, (char*)"CASIO");
-    std::cout << wristwatch2.getDeepWater() << "\n";
-    std::cout << (wristwatch2.getWaterProof() ? "yes" : "no") << "\n";
- 
     return 0;
 }
--- Watch.h
+++ Watch.h
@@ -1,42 +1,7 @@
 #pragma once
-
-#ifndef WATCH_H
-#define WATCH
-
 class Watch
 {
-protected:
-	short _timeHours;
-	short _timeMinutes;
-	char* _time;
-	char* _produced;
 
-	short getTimeHours();
-	short getTimeMinutes();
-	
-
-public:
-	Watch() : _timeHours{}, _timeMinutes{}, _produced{}
-	{
-		_time = getTime();
-	}
-
-	Watch(short timeHours, short timeMinutes, char* produced) {
-		_timeHours = timeHours;
-		_timeMinutes = timeMinutes;
-		_produced = produced;
-		_time = getTime();
-	}
-
-	~Watch() {
-		_produced = nullptr;
-		_time = nullptr;
-	}
-
-	char* getTime();
-
-	void setTimeHours(short timeHours);
-	void setTimeMinutes(short timeMinutes);
 }
 ;
-#endif
+
